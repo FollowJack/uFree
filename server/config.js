97d2a759ac37676e2fa4a672dddc143383bcb245
Deniel Horvatic <deniel.horvatic@porschedigitallab.com>
2018-07-30 00:12:12 +0200

    Added config files

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  CLIENT_DOMAIN: 'followjack.eu.auth0.com', // e.g. 'you.auth0.com'
+  AUTH0_AUDIENCE: 'http://localhost:3001/api',
+  DB_CONNECTION_STRING: 'postgres://localhost:5432/ufree_db_test'
+};
